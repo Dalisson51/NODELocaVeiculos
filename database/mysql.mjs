955c324c074ab481af40910885643356abb451ab
Dálisson Alves <93987387+Dalisson51@users.noreply.github.com>
2024-08-19 12:14:32 -0300

    Update mysql.mjs

--- database/mysql.mjs
+++ database/mysql.mjs
@@ -1,9 +1,7 @@
 import { Sequelize } from "sequelize";
 
 // Substitua a URL de conexão MySQL pela URL do PostgreSQL
-const sequelize = new Sequelize("postgresql://dewii2024_user:6fEiGwJityorYsrJn0pBsUbaRw5kscQM@dpg-cr1m10dds78s739u01a0-a/dewii2024", {
-  dialect: 'postgres',
-});
+const sequelize = new Sequelize("postgresql://dewii2024_user:6fEiGwJityorYsrJn0pBsUbaRw5kscQM@dpg-cr1m10dds78s739u01a0-a/dewii2024");
 
 sequelize.sync();
 
